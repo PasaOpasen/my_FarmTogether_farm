5296d8b68080f413043a2abed2e6544b8ffdb8cb
ДМ. Па <qtckpuhdsa@gmail.com>
2020-08-04 20:10:06 +0300

    python script for screenshots

---
+++ update_readme.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 19:58:33 2020
+
+@author: qtckp
+"""
+
+
+import os
+from os import listdir
+from os.path import isfile, join, dirname
+
+
+screen_shots_dir = join(dirname(__file__), 'screenshots')
+
+
+onlyfiles = [join(screen_shots_dir, f) for f in listdir(screen_shots_dir) if isfile(join(screen_shots_dir, f))]
+
+
+with open('README.md', 'r', encoding = 'utf8') as f:
+    lines = [line for line in f if not line.startswith('!')]
+
+
+onlyfiles = [fr'![1](https://github.com/PasaOpasen/my_FarmTogether_farm/blob/master/screenshots/{file})\n\n' for file in onlyfiles]
+
+
+with open('README.md', 'w', encoding = 'utf8') as f:
+    f.writelines(lines + onlyfiles)
+
+
+
+
+
+
+
+
+
+
+
+
